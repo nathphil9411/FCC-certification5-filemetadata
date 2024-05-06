8fe1e619980b82e656836cae41f5e5f60ad0528c
Ezeanaka Nathaniel Nwabueze <ezeanakanath@gmail.com>
2024-05-06 08:26:21 +0100

    Update final solution

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+const multer = require("multer");
+require("dotenv").config();
 
-var app = express();
+const upload = multer({ dest: "public/img/user" });
+const app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+	res.sendFile(process.cwd() + "/views/index.html");
 });
 
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+	const { originalname, mimetype, size } = req.file;
 
-
-
+	// Respond with JSON containing file details
+	res.json({
+		filename: originalname,
+		filetype: mimetype,
+		sizeInBytes: size,
+	});
+});
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+	console.log("Your app is listening on port " + port);
 });
